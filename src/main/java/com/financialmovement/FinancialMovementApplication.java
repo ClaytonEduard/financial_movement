b4ee92746c9c11ea133fbb48a0901108150d6b2d
ClaytonEduard <clayton_eduard@hotmail.com>
2023-05-23 10:22:20 -0300

    First commit

---
+++ src/main/java/com/financialmovement/FinancialMovementApplication.java
@@ -0,0 +1,13 @@
+package com.financialmovement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialMovementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialMovementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financialmovement/FinancialMovementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialmovement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialMovementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
